d94eda5a7060f191fcb4896cf7eead99c733e70e
LucaTheSorcerer <lucka.tudor@gmail.com>
2022-11-09 11:45:04 +0200

    function for game logic - still to review

--- Aufgabe_1/consoleInput/io_user_computer.py
+++ Aufgabe_1/consoleInput/io_user_computer.py
@@ -3,11 +3,13 @@ import random
 def user_input():
     user_action = input("Enter a choice --> rock, paper, scissors: ")
     return user_action
-print(user_input())
+input_from_user = user_input()
+#print(f"Input from user is {input_from_user}")
 
 def computer_choice():
     possible_actions = ["rock", "paper", "scissors"]
     computer_action = random.choice(possible_actions)
     return computer_action
-print(computer_choice())
+input_from_computer = computer_choice()
+print(f"Input from computer is {input_from_computer}")
 
